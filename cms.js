ca78a2df2ca4005ffa283a7f6e2d16464ec1dd68
prabhatgss <120572475+prabhatgss@users.noreply.github.com>
2022-12-15 18:45:10 +0530

    Add files via upload

---
+++ cms.js
@@ -0,0 +1,60 @@
+let leftArrow = document.querySelector(".leftArrow");
+let rightArrow = document.querySelector(".rightArrow");
+let currMonth = document.querySelector(".Date").children[0].innerText;
+let monthLabel = document.querySelector(".monthLabel");
+leftArrow.addEventListener("click",handleLeftArrow);
+rightArrow.addEventListener("click",handleRightArrow);
+let year = 2022;
+
+
+function handleLeftArrow(){
+    let date = document.querySelectorAll(".Date");
+    for(let i=0;i<date.length;i++){
+     date[i].children[0].innerText = previousMonth(currMonth);
+    }
+    currMonth = previousMonth(currMonth);
+    monthLabel.innerText = currMonth+" "+year;
+}
+
+function handleRightArrow(){
+let date = document.querySelectorAll(".Date");
+   for(let i=0;i<date.length;i++){
+    date[i].children[0].innerText = nextMonth(currMonth);
+   }
+   currMonth = nextMonth(currMonth);
+   monthLabel.innerText = currMonth+" "+year;
+
+}
+
+
+
+function nextMonth(month){
+    if(month=="Jan") return "Feb" 
+    if(month=="Feb") return "Mar" 
+    if(month=="Mar") return "Apr" 
+    if(month=="Apr") return "May" 
+    if(month=="May") return "Jun" 
+    if(month=="Jun") return "Jul" 
+    if(month=="Jul") return "Aug" 
+    if(month=="Aug") return "Sep" 
+    if(month=="Sep") return "Oct" 
+    if(month=="Oct") return "Nov" 
+    if(month=="Nov") return "Dec" 
+    if(month=="Dec") return "Jan" 
+}
+
+
+function previousMonth(month){
+    if(month=="Jan") return "Dec" 
+    if(month=="Feb") return "Jan" 
+    if(month=="Mar") return "Feb" 
+    if(month=="Apr") return "Mar" 
+    if(month=="May") return "Apr" 
+    if(month=="Jun") return "May" 
+    if(month=="Jul") return "Jun" 
+    if(month=="Aug") return "Jul" 
+    if(month=="Sep") return "Aug" 
+    if(month=="Oct") return "Sep" 
+    if(month=="Nov") return "Oct" 
+    if(month=="Dec") return "Nov" 
+}
