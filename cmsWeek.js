5cdafeceb2d9484f3977f2b25128c4c5fb722547
prabhatgss <120572475+prabhatgss@users.noreply.github.com>
2022-12-19 15:42:39 +0530

    Add files via upload

---
+++ cmsWeek.js
@@ -0,0 +1,143 @@
+let myDayNum = 1;
+let year = "";
+let month = "";
+let monthLabel = document.querySelector(".monthLabel");
+function getDayOfCurrentWeek(num) {
+    const today = new Date();
+    const first = today.getDate() - today.getDay() + num;
+    let day = new Date(today.setDate(first));
+    year = String(day).slice(11,15);
+    month = String(day).slice(4,7);
+    day = String(day).slice(3,10);
+    monthLabel.innerText = month+" "+year;
+    return day;
+  }
+
+
+
+  function setDate(para){
+    let allDates = document.querySelectorAll(".Date");
+    if(para=="prev") myDayNum = myDayNum-14;
+    for(let i=0;i<allDates.length;i++){
+       allDates[i].children[0].innerText=getDayOfCurrentWeek(myDayNum);
+       myDayNum++;
+    }
+  }
+setDate("next");
+let leftArrow = document.querySelector(".leftArrow");
+let rightArrow = document.querySelector(".rightArrow");
+leftArrow.addEventListener("click",handleLeftArrow);
+rightArrow.addEventListener("click",handleRightArrow);
+
+
+
+function handleLeftArrow(){
+    setDate("prev");
+}
+   
+
+function handleRightArrow(){
+    setDate("next");
+}
+
+
+
+function nextMonth(month){
+    if(month=="Jan") return "Feb" 
+    if(month=="Feb") return "Mar" 
+    if(month=="Mar") return "Apr" 
+    if(month=="Apr") return "May" 
+    if(month=="May") return "Jun" 
+    if(month=="Jun") return "Jul" 
+    if(month=="Jul") return "Aug" 
+    if(month=="Aug") return "Sep" 
+    if(month=="Sep") return "Oct" 
+    if(month=="Oct") return "Nov" 
+    if(month=="Nov") return "Dec" 
+    if(month=="Dec") return "Jan"
+}
+
+
+function previousMonth(month){
+    if(month=="Jan") return "Dec" 
+    if(month=="Feb") return "Jan" 
+    if(month=="Mar") return "Feb" 
+    if(month=="Apr") return "Mar" 
+    if(month=="May") return "Apr" 
+    if(month=="Jun") return "May" 
+    if(month=="Jul") return "Jun" 
+    if(month=="Aug") return "Jul" 
+    if(month=="Sep") return "Aug" 
+    if(month=="Oct") return "Sep" 
+    if(month=="Nov") return "Oct" 
+    if(month=="Dec") return "Nov" 
+}
+
+
+function changeBorder(){
+    let empStatus = document.querySelectorAll(".emp_status");
+    let mobDate = document.querySelectorAll(".mobDate");
+
+     for(let i=0;i<empStatus.length;i++){
+        if(empStatus[i].innerText=="W"){
+            empStatus[i].style.borderBottom =" #e8c6c6 4px solid"; 
+        }
+
+        if(empStatus[i].innerText=="O"){
+            empStatus[i].style.borderBottom ="#fe4200d1 4px solid"; 
+        }
+    }
+
+
+    for(let i=0;i<mobDate.length;i++){
+        console.log(mobDate[i].children[2].innerText);
+        if(mobDate[i].children[2].innerText == " W"){
+            mobDate[i].children[2].style.borderBottom = "#e8c6c6 4px solid";
+        }
+
+        if(mobDate[i].children[2].innerText == " O"){
+            mobDate[i].children[2].style.borderBottom = "#fe4200d1 4px solid";
+        }
+    }
+
+
+}
+
+
+changeBorder();
+
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction(num) {
+    console.log(num)
+    let myDropdown = document.querySelectorAll(".myDropdown");
+    // document.querySelectorAll(".myDropdown")[num].classList.toggle("show");
+    for(let i=0;i<myDropdown.length;i++){
+        if(i==num){
+            myDropdown[i].classList.toggle("show");
+        }else{
+            myDropdown[i].classList.remove("show");
+        }
+    }
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  
+
+  
